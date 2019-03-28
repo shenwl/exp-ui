f95eae61af3e0ace4f3a6861a5fa1e6080586a93
shenwl <shenweilun96@gmail.com>
2019-03-28 21:56:23 +0800

    init env

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+const { resolve } = path;
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './lib/index.tsx'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/lib'),
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    hot: true,
+    hotOnly: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, './index.html'),
+    }),
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+}
