02c341378d2cf33dc4a9ce72e9a4a1a31bc1ebd4
shenwl <shenweilun96@gmail.com>
2019-03-29 22:00:34 +0800

    index导出所有组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDom from "react-dom";
 import Button from "./button";
 
-ReactDom.render(<Button />, document.getElementById('root'));
+export {
+  Button,
+}
