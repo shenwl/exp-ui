740215a4e40356f50fbca2404ca33f8ce9e07603
shenwl <shenweilun96@gmail.com>
2019-03-31 19:34:33 +0800

    svg rule

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -19,7 +19,7 @@ module.exports = {
         loader: 'awesome-typescript-loader',
       },
       {
-        test: /icons.+\.svg$/,
+        test: /\.svg$/,
         loader: 'svg-sprite-loader',
       }
     ]
