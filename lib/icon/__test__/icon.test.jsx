9dcdc38ccb398cc0a9a71a50a772eee5cd32d3e9
shenwl <shenweilun96@gmail.com>
2019-03-31 23:38:39 +0800

    测试描述修改

--- lib/icon/__test__/icon.test.jsx
+++ lib/icon/__test__/icon.test.jsx
@@ -3,13 +3,13 @@ import * as renderer from 'react-test-renderer';
 import Icon from '../index';
 import { mount } from 'enzyme';
 
-describe('测试Icon组件', () => {
-  it('UI测试', () => {
+describe('Icon', () => {
+  it('render successfully', () => {
     const json = renderer.create(<Icon name='wechat' />).toJSON();
     expect(json).toMatchSnapshot();
   });
 
-  it('onclick', () => {
+  it('onClick', () => {
     const fn = jest.fn();
     const comp = mount(<Icon name='wechat' onClick={fn} />);
     comp.find('svg').simulate('click');
