40a8cbaafd2c208a37e96692f6ee94ab5a2ee708
shenwl <shenweilun96@gmail.com>
2019-03-31 21:45:39 +0800

    让icon可以接收click事件

--- lib/icon/index.tsx
+++ lib/icon/index.tsx
@@ -4,11 +4,12 @@ import './style.scss';
 
 interface IconProps {
   name: string;
+  onClick: React.MouseEventHandler<SVGElement>;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <svg className="exp__icon">
+    <svg className="exp__icon" onClick={props.onClick}>
       <use xlinkHref={`#${props.name}`} />
     </svg>
   );
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Icon from "./icon";
 
+const handleClick: React.MouseEventHandler = (e) => {
+  console.log(e.currentTarget);
+}
+
 ReactDOM.render((
   <div>
-    <Icon name="wechat" />
+    <Icon name="wechat" onClick={handleClick} />
   </div>
 ), document.getElementById('root'));
 
