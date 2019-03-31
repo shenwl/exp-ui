8a4fa60237471d8175582f3c4858909c0d19ac23
shenwl <shenweilun96@gmail.com>
2019-03-31 23:33:22 +0800

    测试时加--env=jsdom，防止enzyme的mount报错

--- lib/icon/__test__/icon.test.jsx
+++ lib/icon/__test__/icon.test.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import * as renderer from 'react-test-renderer';
-import Icon from '../index';
+import React from 'react'
+import * as renderer from 'react-test-renderer'
+import Icon from '../index'
+import { mount } from 'enzyme'
 
 describe('测试Icon组件', () => {
   it('UI测试', () => {
-    const json = renderer.create(<Icon name="wechat"/>).toJSON();
-    expect(json).toMatchSnapshot();
-  });
-});
+    const json = renderer.create(<Icon name='wechat' />).toJSON()
+    expect(json).toMatchSnapshot()
+  })
 
+  it('onclick', () => {
+    let n = 1
+    const fn = () => {
+      n = 2
+    }
+    const comp = mount(<Icon name='wechat' onClick={fn} />)
+    comp.find('svg').simulate('click')
+    expect(n).toEqual(2)
+  })
+})
