62072de3dcbcea53ebc1860dec345cc73879a19f
shenwl <shenweilun96@gmail.com>
2019-03-31 22:45:31 +0800

    删除测试demo

--- test/icon.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Icon from '../lib/icon';
-
-describe('测试组件 Icon', () => {
-  it('Icon是个div', () => {
-    
-  });
-});
