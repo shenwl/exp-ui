47407d550632fe4d6ea77757b232768327323b2b
shenwl <shenweilun96@gmail.com>
2019-03-31 19:44:17 +0800

    初步实现Icon组件功能

--- lib/icon/index.tsx
+++ lib/icon/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import "../../icons/wechat.svg";
 
 interface IconProps {
-  name: string,
+  name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
+const Icon: React.FunctionComponent<IconProps> = props => {
   return (
-    <span>Icon {props.name}</span>
-  )
-}
+    <span className="exp__icon">
+      <svg>
+        <use xlinkHref={`#${props.name}`} />
+      </svg>
+    </span>
+  );
+};
 
 export default Icon;
