7c09753243274b3cf2e7427e65aa33a3fc0718ba
shenwl <shenweilun96@gmail.com>
2019-03-31 22:10:37 +0800

    抽离出classNames函数

--- lib/icon/index.tsx
+++ lib/icon/index.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import "../../icons/importAll";
 import './style.scss';
 
+import classNames from '../helpers/classnames';
+
 interface IconProps extends React.SVGAttributes<SVGElement> {
   name: string;
 }
 
 const Icon: React.FunctionComponent<IconProps> = props => {
   const {className, ...restProps} = props;
+  
   return (
     <svg 
-      className={`exp__icon${className ? (' ' + className) : ''}`} 
+      className={classNames('exp__icon', className)} 
       {...restProps}
     >
       <use xlinkHref={`#${props.name}`} />
