c27b23508372f35fc9e1c9c2e282e6335713175f
shenwl <shenweilun96@gmail.com>
2019-03-31 23:37:26 +0800

    typo

--- lib/icon/__test__/icon.test.jsx
+++ lib/icon/__test__/icon.test.jsx
@@ -5,14 +5,14 @@ import { mount } from 'enzyme';
 
 describe('测试Icon组件', () => {
   it('UI测试', () => {
-    const json = renderer.create(<Icon name='wechat' />).toJSON()
-    expect(json).toMatchSnapshot()
-  })
+    const json = renderer.create(<Icon name='wechat' />).toJSON();
+    expect(json).toMatchSnapshot();
+  });
 
   it('onclick', () => {
     const fn = jest.fn();
     const comp = mount(<Icon name='wechat' onClick={fn} />);
     comp.find('svg').simulate('click');
     expect(fn).toBeCalled();
-  })
-})
+  });
+});
