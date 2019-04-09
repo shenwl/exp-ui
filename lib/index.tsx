4c0a33f1eb1664122d08f53cdf335ffa33dfc4d3
shenwl <shenweilun96@gmail.com>
2019-04-09 22:35:46 +0800

    files配置修改,index导出组件

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,21 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Icon from "./icon";
 
-const handleClick: React.MouseEventHandler = (e) => {
-  console.log(e.currentTarget);
+export {
+  Icon,
 }
 
-ReactDOM.render((
-  <div>
-    <Icon 
-      name="wechat" 
-      className="qq"
-      onClick={handleClick} 
-      onMouseEnter={() => {console.log('enter')}} 
-      onMouseLeave={() => {console.log('leave')}} 
-    />
-  </div>
-), document.getElementById('root'));
 
  
