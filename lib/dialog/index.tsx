e017625c748ae168fa63f7450bef46fdfb7ce3d2
shenwl <shenweilun96@gmail.com>
2019-05-23 23:01:34 +0800

    close && maskClosable

--- lib/dialog/index.tsx
+++ lib/dialog/index.tsx
@@ -1,37 +1,52 @@
-import React, {Fragment} from "react";
+import React, {Fragment, ReactChildren, ReactElement} from "react";
 import Icon from '../icon';
-import "./style.scss";
+import './style.scss';
 import {scopedClassMaker} from '../helpers/classes';
 
 const sc = scopedClassMaker('exp__dialog')
 
 interface Props {
   visible: Boolean;
-  onOk: Function;
-  onCancel: Function;
-  onConfirm: Function;
+  onClose: React.MouseEventHandler;
+  maskClosable?: Boolean;
+  onOk?: Function;
+  onCancel?: Function;
+  onConfirm?: Function;
   closable?: Boolean;
   mask?: Boolean;
+  children?: ReactElement | Element | String;
+  footer?: ReactElement | Element | String;
 }
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
-  const { onCancel} = props;
+
+  const handleClose: React.MouseEventHandler = (e) => {
+    props.onClose(e);
+  }
+  const handleClickMask: React.MouseEventHandler = (e) => {
+    props.maskClosable && props.onClose(e);
+  }
   return (
     props.visible ? (
       <Fragment>
+        <div className={sc('mask')} onClick={handleClickMask}></div>
+
         <div className={sc()}>
           <header className={sc('header')}>
             <span>提示</span>
-            <Icon name="close" onClick={onCancel} />
+            <div className={sc('close')} onClick={handleClose}>
+              <Icon name="close" />
+            </div>
           </header>
 
           <main className={sc('content')}>
             { props.children }
           </main>
 
-          <footer className={sc('footer')}></footer>
+          <footer className={sc('footer')}>
+            { props.footer }
+          </footer>
         </div>
-        <div className={sc('mask')}></div>
       </Fragment>
     ): null
   );
@@ -40,6 +55,9 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
 Dialog.defaultProps = {
   closable: true,
   mask: true,
+  maskClosable: false,
+  children: '',
+  onClose: () => {},
 };
 
 export default Dialog;
