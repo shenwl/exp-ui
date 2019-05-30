dbab7a0086956f296c415d5230ab0da18a2f0769
shenwl <shenweilun96@gmail.com>
2019-05-30 21:25:51 +0800

    去除无用引入

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from "react";
+import React, { ReactElement } from "react";
 import { classNames, scopedClassMaker } from "../helpers/classes";
 import Sider from './sider';
 import "./style.scss";
