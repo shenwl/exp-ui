e1cc54bbded5444b2b69765bfad1b57867ec93c4
shenwl <shenweilun96@gmail.com>
2019-05-31 22:53:45 +0800

    layout example样式优化

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -1,53 +1,69 @@
 import React from "react";
 import { Layout, Header, Footer, Content, Sider } from './index';
 
+const baseStyle = {
+  padding: 10,
+  margin: 10,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: '#ccc',
+}
+const headerStyle = {
+  ...baseStyle,
+  height: 60,
+}
+
+const footerStyle = { ...headerStyle };
+
+const siderStyle = {
+  ...baseStyle,
+  width: '30%',
+}
+
+const contentStyle = {
+  ...baseStyle,
+}
+
+const layoutStyle = {
+  height: 500,
+  padding: 10,
+  border: '1px solid #ccc',
+}
+
 export default  () => {
   return (
     <div>
-      <Layout style={{
-        height: 500,
-      }}>
-        <Header>header</Header>
-        <Content>content</Content>
-        <Footer>footer</Footer>
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>header</Header>
+        <Content style={contentStyle}>content</Content>
+        <Footer style={footerStyle}>footer</Footer>
       </Layout>
       
-      <Layout style={{
-        height: 500,
-      }}>
-        <Header>header2</Header>
-        <Layout style={{
-          height: 500,
-        }}>
-          <Sider>sider2</Sider>
-          <Content>content2</Content>
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>header2</Header>
+        <Layout>
+          <Sider style={siderStyle}>sider2</Sider>
+          <Content style={contentStyle}>content2</Content>
         </Layout>
-        <Footer>footer2</Footer>
+        <Footer style={footerStyle}>footer2</Footer>
       </Layout>
          
-      <Layout style={{
-        height: 500,
-      }}>
-        <Header>header3</Header>
-        <Layout style={{
-          height: 500,
-        }}>
-          <Content>content3</Content>
-          <Sider>sider3</Sider>
+      <Layout style={layoutStyle}>
+        <Header style={headerStyle}>header3</Header>
+        <Layout>
+          <Content style={contentStyle}>content3</Content>
+          <Sider style={siderStyle}>sider3</Sider>
         </Layout>
-        <Footer>footer3</Footer>
+        <Footer style={footerStyle}>footer3</Footer>
       </Layout>
 
-      <Layout style={{
-        height: 500,
-      }}>
-        <Sider>sider4</Sider>
-        <Layout style={{
-          height: 500,
-        }}>
-          <Header>header4</Header>
-          <Content>content4</Content>
-          <Footer>footer4</Footer>
+      <Layout style={layoutStyle}>
+        <Sider style={siderStyle}>sider4</Sider>
+        <Layout>
+          <Header style={headerStyle}>header4</Header>
+          <Content style={contentStyle}>content4</Content>
+          <Footer style={footerStyle}>footer4</Footer>
         </Layout>
       </Layout>
     </div>
