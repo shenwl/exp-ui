2e1dd33fbe2d8c83f13de079b6c1d746ee9c759a
shenwl <shenweilun96@gmail.com>
2019-06-01 14:43:06 +0800

    使用prism-react-renderer代码高亮

--- example/codeDemo.tsx
+++ example/codeDemo.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react';
+import Highlight, { defaultProps } from 'prism-react-renderer';
 
 interface Props {
   children: ReactElement,
@@ -9,9 +10,19 @@ const CodeDemo: React.FunctionComponent<Props> = (props) => {
   return (
     <div>
       { props.children }
-      <pre>
-        { props.code }
-      </pre>
+      <Highlight {...defaultProps} code={props.code} language="jsx">
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        )}
+      </Highlight>
     </div>
   )
 }
