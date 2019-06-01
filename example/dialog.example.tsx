7b049cb50c66bee9a32921e1b1dac6ddaf45b874
shenwl <shenweilun96@gmail.com>
2019-06-01 11:52:13 +0800

    官网雏形

--- example/dialog.example.tsx
+++ example/dialog.example.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { alert, confirm, modal } from '../lib/dialog/index';
+import { alert, confirm, modal, Button } from '../lib/index';
 
 const DialogExample: React.FunctionComponent = () => {
   return (
     <div>
-      <button onClick={() => { alert({ content: 'alert', title: 'alert' }) }}>alert</button>
-      <button onClick={() => { confirm({ content: 'alert', title: 'confirm' }) }}>confirm</button>
-      <button onClick={() => { modal({ content: 'modal', title: 'modal' }) }}>modal</button>
+      <Button onClick={() => { alert({ content: 'alert', title: 'alert' }) }}>alert</Button>
+      <Button onClick={() => { confirm({ content: 'alert', title: 'confirm' }) }}>confirm</Button>
+      <Button onClick={() => { modal({ content: 'modal', title: 'modal' }) }}>modal</Button>
     </div>
   )
 };
--- example/index.tsx
+++ example/index.tsx
@@ -8,21 +8,25 @@ import LayoutExample from './layout.example';
 
 import { Layout, Header, Footer, Content, Sider } from '../lib/layout/index';
 
+const logo = require('./logo.png');
+
 import './index.scss';
 
 ReactDOM.render((
   <Router>
     <Layout className="page-wrapper">
-      <Header>
+      <Header className="header-wrapper">
         <div className="logo">
-          <h1>Exp UI</h1>
+          <img src={logo} alt="logo"/>
         </div>
       </Header>
 
-      <Layout>
-        <Sider>
-          <h2>组件</h2>
-          <ul>
+      <Layout className="main-wrappper">
+        <Sider className="sider-wrapper">
+          <ul className="sider-list">
+            <li>
+              <h2>组件</h2>
+            </li>
             <li>
               <Link to="/icon">Icon</Link>
             </li>
@@ -35,14 +39,13 @@ ReactDOM.render((
           </ul>
         </Sider>
 
-        <Content>
+        <Content className="content-wrapper">
           <Route path="/icon" component={IconExample}></Route>
           <Route path="/dialog" component={DialogExample}></Route>
           <Route path="/layout" component={LayoutExample}></Route>
         </Content>
       </Layout>
       <Footer>
-        <strong>coding by shenwl.</strong>
       </Footer>
     </Layout>
   </Router>
